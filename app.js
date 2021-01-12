af3d42316c25c6201301b9ee27d6e8d4b5777c71
jan <jansoriano.programming@gmail.com>
2021-01-12 17:29:06 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
+dotenv.config({path: './config/config.env' })
+connectDB()
+const app = express()
+if (process.env.NODE_ENV==='development') {
+
+    app.use(morgan('dev'))
+}
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
