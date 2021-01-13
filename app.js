a53bd89ac23217e3311e4a170ea02af5e013ac46
jan <jansoriano.programming@gmail.com>
2021-01-13 12:11:11 +0800

    template layout engine,views,routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 dotenv.config({path: './config/config.env' })
 connectDB()
@@ -9,6 +11,17 @@ if (process.env.NODE_ENV==='development') {
 
     app.use(morgan('dev'))
 }
+//handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//static folder
+
+app.use(express.static(path.join(__dirname,'public')))
+
+//routes
+
+app.use('/',require('./routes/index'))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
